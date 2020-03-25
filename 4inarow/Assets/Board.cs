1e7dcff2160668c709bbdf224ffccb072183bc5a
lokeshkumar1996 <59974448+lokeshkumar1996@users.noreply.github.com>
2020-03-25 08:55:22 +0000

    2 player function

--- 4inarow/Assets/Board.cs
+++ 4inarow/Assets/Board.cs
@@ -5,27 +5,63 @@ using System;
 
 public class Board : MonoBehaviour
 {
+    public static Board instance;
+
     public GameObject boardtile;
     public GameObject maincamera;
+    public GameObject redpiece;
+    public GameObject yellopiece;
 
     public int noOfRow ;
-    public int noOfCol ;
+    public int noOfCol ;   
+    
+    public GameObject[,] piece; 
+    
+    public int clickedpieceX ;
+    public int clickedpieceY ;
+    public int lastcoinplacedX ;
+    public int lastcoinplacedY ;
+    
 
+    public bool playerturn = true;
+
+    private int[] xcordinatesfilled;
+    
+    /*void Awake()
+    {      
+
+        if(instance == null)
+            instance =this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+      
+    }*/
+
+
+   
     // Start is called before the first frame update
     void Start()
     {
-
+        instance =this;       
         genarateBoard(noOfRow,noOfCol);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update () 
     {
-        
-    }
+
+
+    } 
+
 
     private void genarateBoard(int x, int y)
     {
+        piece = new GameObject[x,y];
+        xcordinatesfilled = new int[x];
         float maincameraXcord = 0;
         float maincameraYcord = 0;
 
@@ -33,12 +69,131 @@ public class Board : MonoBehaviour
         {
             for(int j=0; j<y; j++)
             {
-                Instantiate(boardtile, new Vector3(i * 10F, j*10f, 0f), Quaternion.identity); 
-                maincameraYcord = j;  
+               piece[i,j] = Instantiate(boardtile, new Vector3(i * 10F, j*10f, 0f), Quaternion.identity);
+               piece[i,j].transform.parent = gameObject.transform;
+               piece[i,j].GetComponent<boardPiece>().xCord = i;
+               piece[i,j].GetComponent<boardPiece>().yCord = j;
+               maincameraYcord = j;
             }
             maincameraXcord = i;
         }
-       maincamera.transform.position = new Vector3(maincameraXcord/2 * 10F, maincameraYcord/2 * 10F, -(maincameraXcord * 20f /(float)Math.Sqrt(3)));
-       //maincamera.transform.position = new Vector3(maincameraXcord/2 * 10F, maincameraYcord/2 * 10F, -(maincameraXcord * 20F )+ maincameraXcord Math.Sqrt(3)/2); 
+  
+      // maincamera.transform.position = new Vector3(maincameraXcord/2 * 10F, maincameraYcord/2 * 10F, -(maincameraXcord * 20f /(float)Math.Sqrt(3)));
+       maincamera.transform.position = new Vector3(maincameraXcord/2 * 10F, maincameraYcord/2 * 10F, -(maincameraXcord * 20F)); 
     }
+
+
+
+
+
+    public void spawnplayercoin()
+    {
+        if(playerturn)
+        {
+
+        Instantiate(yellopiece, new Vector3(clickedpieceX * 10F, xcordinatesfilled[clickedpieceX]*10f, 0f), Quaternion.identity);
+        piece[clickedpieceX,xcordinatesfilled[clickedpieceX]].GetComponent<boardPiece>().coin=1;
+        lastcoinplacedX=clickedpieceX;
+        lastcoinplacedY=xcordinatesfilled[clickedpieceX];
+        xcordinatesfilled[clickedpieceX] += 1;        
+        }
+        else
+        {
+         Instantiate(redpiece, new Vector3(clickedpieceX * 10F, xcordinatesfilled[clickedpieceX]*10f, 0f), Quaternion.identity);
+         piece[clickedpieceX,xcordinatesfilled[clickedpieceX]].GetComponent<boardPiece>().coin=2;
+         lastcoinplacedX=clickedpieceX;
+         lastcoinplacedY=xcordinatesfilled[clickedpieceX];
+         xcordinatesfilled[clickedpieceX] += 1;         
+        }
+    }
+
+
+    public void checkwinning(bool player)
+    {
+        int coinvalue;
+
+        if(player)
+        coinvalue =1;
+        else coinvalue =2;
+
+        //checking rightside
+        if(lastcoinplacedX < noOfRow-3)
+        {
+        if(piece[lastcoinplacedX+1,lastcoinplacedY].GetComponent<boardPiece>().coin == coinvalue)
+        {
+           if(piece[lastcoinplacedX+2,lastcoinplacedY].GetComponent<boardPiece>().coin == coinvalue)
+           {
+               if(piece[lastcoinplacedX+3,lastcoinplacedY].GetComponent<boardPiece>().coin == coinvalue)
+               {
+                   Debug.Log("player"+coinvalue+" win");
+               }
+           }
+        }
+        }
+
+        //checking leftside
+        if(lastcoinplacedX > 3)
+        {
+        if(piece[lastcoinplacedX-1,lastcoinplacedY].GetComponent<boardPiece>().coin == coinvalue)
+        {
+           if(piece[lastcoinplacedX-2,lastcoinplacedY].GetComponent<boardPiece>().coin == coinvalue)
+           {
+               if(piece[lastcoinplacedX-3,lastcoinplacedY].GetComponent<boardPiece>().coin == coinvalue)
+               {
+                   Debug.Log("player"+coinvalue+" win");
+               }
+           }
+        }
+        }
+
+        //checking down
+        if(lastcoinplacedY>3)
+        {
+         if(piece[lastcoinplacedX,lastcoinplacedY-1].GetComponent<boardPiece>().coin == coinvalue)
+        {
+           if(piece[lastcoinplacedX,lastcoinplacedY-2].GetComponent<boardPiece>().coin == coinvalue)
+           {
+               if(piece[lastcoinplacedX,lastcoinplacedY-3].GetComponent<boardPiece>().coin == coinvalue)
+               {
+                   Debug.Log("player"+coinvalue+" win");
+               }
+           }
+        }
+        }
+
+        //checking right diagonal
+        if(lastcoinplacedY >3 && lastcoinplacedX < noOfRow-3)
+        {
+         if(piece[lastcoinplacedX+1,lastcoinplacedY-1].GetComponent<boardPiece>().coin == coinvalue)
+        {
+           if(piece[lastcoinplacedX+2,lastcoinplacedY-2].GetComponent<boardPiece>().coin == coinvalue)
+           {
+               if(piece[lastcoinplacedX+3,lastcoinplacedY-3].GetComponent<boardPiece>().coin == coinvalue)
+               {
+                   Debug.Log("player"+coinvalue+" win");
+               }
+           }
+        }
+        }
+
+        //checking left diagonal
+        if(lastcoinplacedY > 3 && lastcoinplacedX > noOfRow+3)
+        {
+         if(piece[lastcoinplacedX-1,lastcoinplacedY-1].GetComponent<boardPiece>().coin == coinvalue)
+        {
+           if(piece[lastcoinplacedX-2,lastcoinplacedY-2].GetComponent<boardPiece>().coin == coinvalue)
+           {
+               if(piece[lastcoinplacedX-3,lastcoinplacedY-3].GetComponent<boardPiece>().coin == coinvalue)
+               {
+                   Debug.Log("player"+coinvalue+" win");
+               }
+           }
+        }
+        }
+
+
+    }
+
+
+    
 }
---
+++ 4inarow/Assets/boardPiece.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boardPiece : MonoBehaviour
+{
+    public int xCord;
+    public int yCord;
+    public int coin =0;
+   
+   // private SpriteRenderer myRenderer;  // 0 no coind, 1 player coind, 2 cpu coin
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Board.instance.clickedpieceX = xCord;
+        Board.instance.clickedpieceY = yCord;
+        Board.instance.spawnplayercoin();
+        
+        if(Board.instance.playerturn)
+        {
+        Board.instance.checkwinning(Board.instance.playerturn);    
+        Board.instance.playerturn = false;
+        }
+        else
+        {
+        Board.instance.checkwinning(Board.instance.playerturn);
+        Board.instance.playerturn = true; 
+        }
+
+    }
+
+    void OnMouseOver()
+    {
+        
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();//SpriteRenderer renderer = GetComponent<SpriteRenderer>();//Get the renderer via GetComponent or have it cached previously
+        renderer.color = new Color(0.5f, 0.5f, 0.5f, 1f);
+    }
+     void OnMouseExit()
+    {
+         SpriteRenderer renderer = GetComponent<SpriteRenderer>();//Get the renderer via GetComponent or have it cached previously
+        renderer.color = new Color(1f, 1f, 1f, 1f);
+    }
+    
+    
+
+
+}
